1d680e78ff926a5baafdfd0b40a866001e0f3d05
tao0213 <znhooo0213@gmail.com>
2021-04-05 21:57:46 +0800

    Refactor code into pipeline design pattern

---
+++ yt_concate/pipeline/steps/get_video_list.py
@@ -0,0 +1,36 @@
+import urllib.request
+import json
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+response = urllib.request.urlopen('https://www.python.org')
+
+from yt_concate.pipeline.steps.step import Step
+from yt_concate.settings import API_KEY
+
+
+class GetVideoList(Step):
+    def process(self, data, inputs):
+        channel_id = inputs['channel_id']
+        base_video_url = 'https://www.youtube.com/watch?v='
+        base_search_url = 'https://www.googleapis.com/youtube/v3/search?'
+
+        first_url = base_search_url + 'key={}&channelId={}&part=snippet,id&order=date&maxResults=25'.format(API_KEY,
+                                                                                                            channel_id)
+
+        video_links = []
+        url = first_url
+        while True:
+            inp = urllib.request.urlopen(url)
+            resp = json.load(inp)
+
+            for i in resp['items']:
+                if i['id']['kind'] == "youtube#video":
+                    video_links.append(base_video_url + i['id']['videoId'])
+
+            try:
+                next_page_token = resp['nextPageToken']
+                url = first_url + '&pageToken={}'.format(next_page_token)
+            except KeyError:
+                break
+        print(video_links)
+        return video_links
